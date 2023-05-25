ee55a43cfafa5271bce53997118f66a42e710128
ChristianPaul123 <christianpaulespares1@gmail.com>
2023-05-25 14:18:57 +0800

    version 34

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let menu = document.querySelector('#menu-bars');
 let header = document.querySelector('header');
+const img = document.querySelector("img");
 
 menu.onclick = () => {
   menu.classList.toggle('fa-times');
@@ -11,3 +12,54 @@ window.onscroll = () =>{
   header.classList.remove('active');
 }
 
+
+img.onclick = function(){
+  this.classList.toggle("active");
+}
+
+//responsive gallery
+
+const allImages = document.querySelectorAll(".images .img");
+const lightbox = document.querySelector(".cover");
+const closeImgBtn = lightbox.querySelector(".close-icon");
+
+allImages.forEach(img => {
+    //  showLightBox function  passing clicked img src as argument
+    img.addEventListener("click", () => showLightbox(img.querySelector("img").src));
+});
+
+const showLightbox = (img) => {
+    // Showing lightbox and updating
+    lightbox.querySelector("img").src = img;
+    lightbox.classList.add("show");
+    document.body.style.overflow = "hidden";
+}
+
+closeImgBtn.addEventListener("click", () => {
+    // Hiding lightbox on close 
+    lightbox.classList.remove("show");
+    document.body.style.overflow = "auto";
+});
+
+//responsive video gallery
+
+const row2 = document.querySelector(".row-2"),
+mainVideo = document.querySelector(".main-video video"),
+ videos = document.querySelectorAll(".videos"),
+ close = document.querySelector(".close");
+
+ for (var i = 0; i < videos.length; i++) {
+   videos[i].addEventListener("click", (e)=>{
+     const target = e.target;
+     row2.classList.add("active");
+     target.classList.add("active");
+     let src = document.querySelector(".videos.active video").src;
+     mainVideo.src = src;
+
+     close.addEventListener("click", ()=>{
+       row2.classList.remove("active");
+       target.classList.remove("active");
+       mainVideo.src = "";
+     });
+   });
+ };
